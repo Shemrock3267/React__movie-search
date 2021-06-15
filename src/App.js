d06e40e9f7439cd132c950b554c412eef2bb8408
Roman Oliinyk <dev.oliinyk@gmail.com>
2021-06-15 18:48:27 +0300

    basic styling

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1>Movie search app will be here</h1>
-    </>
-  );
-}
-
-export default App;
---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Main extends Component() {
+  state = {};
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Movie search app</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
